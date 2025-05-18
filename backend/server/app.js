b094b9142828e04c50e34c4e9a1ce7fd9702d544
Sebastian Mora <133081239+Sebas2203@users.noreply.github.com>
2025-05-18 16:00:48 -0600

    Controllers

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,10 +1,13 @@
 //configuration of the express server
-
 import express from "express";
 import postRoutes from "./routes/posts.routes.js";
 
 const app = express();
 
+//middleware
+app.use(express.json());
+
+//routes
 app.use(postRoutes);
 
 export default app;
--- backend/server/controller/posts.controllers.js
+++ backend/server/controller/posts.controllers.js
@@ -1,5 +1,46 @@
-export const getPosts = (req, res) => res.send([]);
-export const createPost = (req, res) => res.send("New post created");
-export const updatePost = (req, res) => res.send("updating a post");
-export const deletePost = (req, res) => res.send("deleting a post");
-export const getPost = (req, res) => res.send("getting a post");
+import { request } from "express";
+import Post from "../models/Post.js";
+
+//get posts
+export const getPosts = async (req, res) => {
+  const posts = await Post.find();
+  res.send([posts]);
+};
+
+//create post
+export const createPost = async (req, res) => {
+  const { title, description } = req.body;
+  const newPost = new Post({ title, description });
+
+  await newPost.save();
+  return res.json(newPost);
+};
+
+//update post
+export const updatePost = async (req, res) => {
+  // const post = await Post.updateOne({ _id: req.params.id }, req.body, {
+  //   new: true,
+  // });
+  const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  console.log(post);
+  return res.send("received");
+};
+
+//delete post
+export const deletePost = async (req, res) => {
+  const postRemove = await Post.findByIdAndDelete(req.params.id, req.body, {
+    new: true,
+  });
+
+  if (!postRemove) return res.sendStatus(404);
+  return res.sendStatus(204);
+};
+
+//get post by id
+export const getPost = async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  if (!post) return res.sendStatus(404);
+  return res.json(post);
+};
